fd66759fc7aa4b1e234a244ab1ffc9de134f5f63
Dan Seminara <semi225599@gmail.com>
2014-10-11 19:03:36 -0400

    Log phrases to mongo

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+from datetime import datetime
 from pymongo import MongoClient
 from flask import Flask, request
 from uuid import uuid4
@@ -13,15 +14,21 @@ def get_uuid():
     
 @app.route('/search/<uid>', methods=['POST', 'GET'])
 def get_phrase(uid):
-    # Get the phrase from document
-    print(request.args)
-    print(request.data)
-    print(request.form)
-    # db.data.update({'uuid': uid}, {'$push': {'searches': phrase}}, upsert = True)
+    phrase = request.data
+    db.data.update({'uuid': uid}, {'$push': {'searches': phrase.decode()}}, upsert = True)
     return ""
 
+@app.route('/schedule', methods=['POST', 'GET'])
+def schedule():
+    pass # Run scraping for search terms
+    # db.data.update({'uuid': uid}, {'$set': {'last update': datetime.now()}})
+
+@app.route('/info/<uid>', methods=['POST', 'GET'])
+def get_info(uid):
+    pass # return info for given user
+    
 if __name__ == '__main__':
     client = MongoClient()
-    db = client.data_finder
+    db = client.test_db
     
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
