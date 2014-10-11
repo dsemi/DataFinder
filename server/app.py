ae1933969a1529e8d04aaea0cdb162efd65abeec
Dan Seminara <semi225599@gmail.com>
2014-10-11 17:22:38 -0400

    Add template code

---
+++ server/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from flask import Flask
+from uuid import uuid4 as uuid
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return 'Hello worlds'
+
+@app.route('/getid', methods=['POST'])
+def get_uuid():
+    return uuid()
+    
+@app.route('/<uid>')
+def get_phrase(uid):
+    pass
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
